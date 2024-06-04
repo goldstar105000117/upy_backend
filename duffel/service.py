13b32ba564be98138f511a35ac82a1678d5bbe9f
goldstar105000117 <goldstar105000117@gmail.com>
2024-06-04 00:16:40 -0700

    feat: create get aircrafts list endpoint

--- duffel/service.py
+++ duffel/service.py
@@ -31,3 +31,21 @@ def get_airline_by_id(id):
     
     response = requests.get(url, headers=headers, params=params)
     return response.json()
+
+def get_aircrafts(after=None, limit=None):
+    url = "https://api.duffel.com/air/aircraft"
+    headers = {
+        "Accept-Encoding": "gzip",
+        "Accept": "application/json",
+        "Duffel-Version": "v1",
+        "Authorization": f"Bearer {settings.DUFFEL_ACCESS_TOKEN}"
+    }
+    params = {}
+    if after is not None:
+        params["after"] = after
+        params["before"] = after
+    if limit is not None:
+        params["limit"] = limit
+    
+    response = requests.get(url, headers=headers, params=params)
+    return response.json()
--- duffel/urls.py
+++ duffel/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('airlines/', views.get_airlines_view, name='get_airlines'),
     path('airlines/<str:pk>/', views.get_airline_view, name='get_airline_by_id'),
+    path('aircraft/', views.get_aircrafts_view, name='get_aircrafts'),
 ]
