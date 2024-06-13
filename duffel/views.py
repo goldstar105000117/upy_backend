8cf00dd1f461f599161239a182427a9de7b219ec
goldstar105000117 <goldstar105000117@gmail.com>
2024-06-12 23:59:36 -0700

    feat: create get change request endpoint

--- duffel/views.py
+++ duffel/views.py
@@ -2,7 +2,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 from django.http import JsonResponse
 from .decorator import require_auth
-from .service import get_duffel, get_order_cancellation, confirm_order_cancellation, create_cancelled_orders, get_cancelled_orders, get_seats_by_order_id, add_service_to_order, update_order,create_payment, get_orders, get_order_by_id, get_available_services_by_order_id, create_order, update_passenger_details, get_offers, get_offer_by_id, create_duffel_offer_request, get_offer_request_by_id, get_airlines, get_airline_by_id, get_aircrafts, get_aircraft_by_id, get_airports, get_airport_by_id, get_cities, get_city_by_id, get_places, get_offer_requests
+from .service import get_duffel, get_order_cancellation, get_order_change_request, confirm_order_cancellation, create_cancelled_orders, get_cancelled_orders, get_seats_by_order_id, add_service_to_order, update_order,create_payment, get_orders, get_order_by_id, get_available_services_by_order_id, create_order, update_passenger_details, get_offers, get_offer_by_id, create_duffel_offer_request, get_offer_request_by_id, get_airlines, get_airline_by_id, get_aircrafts, get_aircraft_by_id, get_airports, get_airport_by_id, get_cities, get_city_by_id, get_places, get_offer_requests
 import json
 
 @csrf_exempt
@@ -546,3 +546,12 @@ def get_order_cancellation_view(request):
         return JsonResponse({'success': False, 'error': 'No data found'}, status=404)
 
     return JsonResponse({'success': True, 'result': order_cancellation_data})
+
+@csrf_exempt
+@require_http_methods(["POST"])
+# @require_auth
+def get_order_change_request_view(request, pk):
+    order_change_request_data = get_order_change_request(id=pk)
+    if order_change_request_data.get('errors'):
+        return JsonResponse({'success': False, 'error': order_change_request_data['errors'][0]['title']}, status=404)
+    return JsonResponse({'success': True, 'result': order_change_request_data})
